2d9c978d7ffee6a503aa22fccca3413996a2754e
athdot <athdot@gmail.com>
2017-03-22 13:24:51 -0600

    Update que.js

--- que.js
+++ que.js
@@ -1,2 +1,6 @@
-window.location = "https://www.google.com"
 var on = "off"
+if(on === "on"){
+  window.location = "https://athdot.github.io/googl/"
+}else{
+  window.location = "https://www.google.com"
+}
